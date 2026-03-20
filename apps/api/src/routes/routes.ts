ddb84ba85e07b8f9d874d1b2066079ebf1e28187
thisdev-davi <davisouzaoliveira1975@gmail.com>
2026-03-20 00:33:51 -0300

    feat/BK0001 - postsRoutes

--- apps/api/src/routes/routes.ts
+++ apps/api/src/routes/routes.ts
@@ -3,6 +3,6 @@ import usersRoutes from './users.ts'
 import postsRoutes from '../posts/posts.controllers.ts'
 
 export async function routes(app: FastifyTypedInstance) {
-    await app.register(usersRoutes, {prefix: 'api'});
-    await app.register(postsRoutes, {prefix: 'api'})
+    await app.register(usersRoutes, {prefix: 'api/users'});
+    await app.register(postsRoutes, {prefix: 'api/posts'})
 }
