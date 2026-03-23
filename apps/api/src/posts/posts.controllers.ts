55a2db22f189975c8f1791549dec2b20f0193e43
thisdev-davi <davisouzaoliveira1975@gmail.com>
2026-03-23 00:59:28 -0300

    feat/BK0001 - get for id

--- apps/api/src/posts/posts.controllers.ts
+++ apps/api/src/posts/posts.controllers.ts
@@ -2,9 +2,10 @@ import { FastifyTypedInstance } from "../types.ts";
 import { PaginationPosts, PostsSchema, PostsSchemaDb } from "./posts.model.ts";
 import z from "zod";
 import { prisma } from "@repo/database";
+import { request } from "node:http";
 
 export default async function routes(app: FastifyTypedInstance){
-    app.get("/posts", {
+    app.get('/posts', {
         schema: {
             tags: ['Posts'],
             description: 'List Posts',
@@ -28,8 +29,31 @@ export default async function routes(app: FastifyTypedInstance){
     })
 
     // get para listagem única /posts/:id
+    app.get('/posts/:id', {
+        schema: {
+            tags: ['Posts'],
+            description: 'Get Post for id',
+            response: {
+                200: PostsSchemaDb
+            }
+        },
+    }, async (request, reply) => {
+        try {
+        const { id }= request.params as {id: string}
 
-    app.post("/posts", {
+        const post = await prisma.posts.findUnique({
+            where: { id: id},
+        })
+        if(!post) {
+            throw new Error('Post não encontrado')
+        }
+        return reply.status(200).send(post)
+        } catch (erro){
+            console.log(erro)
+        }
+    })
+
+    app.post('/posts', {
         schema: {
             tags: ['Posts'],
             description: 'Create Post',
@@ -52,7 +76,7 @@ export default async function routes(app: FastifyTypedInstance){
                 user_id: user_id
             }
         })
-        console.log(`Post criado, ${newPost}`)
+        console.log(`Post criado, ${newPost.id}`)
         return reply.status(201).send(newPost)
         } catch (erro) {
             console.log(erro)
