a8add5715c710b4d9f1687d7dfb8a02a91effcbd
wladimir <wladimir4019@gmail.com>
2026-03-23 21:58:35 -0300

    chore: add `prettier`

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["@repo/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
+  extends: ['@repo/eslint-config/next.js'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
   },
