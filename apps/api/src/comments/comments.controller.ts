ebf6fa19a9ca6678d12c57332448d3b090379b8f
thisdev-davi <davisouzaoliveira1975@gmail.com>
2026-03-26 00:49:36 -0300

    feat/BK0002 - methods get and posts

--- apps/api/src/comments/comments.controller.ts
+++ apps/api/src/comments/comments.controller.ts
@@ -1,35 +1,95 @@
-import { request } from "node:http";
 import { FastifyTypedInstance } from "../types.ts";
 import { CommentsSchemaDb, CommentsSchema } from "./comments.model.ts";
-import { z } from 'zod'
+import { PaginationPosts } from "../utils/pagination.model.ts"
+import  z  from 'zod'
 import { prisma } from "@repo/database";
+import { request } from "node:http";
 
 export default async function route(app: FastifyTypedInstance) {
     app.post('/comments', {
         schema: {
-            tags: ['Comments'],
+            tags: ['Comments', ],
             description: 'Create Comments',
-            body: CommentsSchema,
+            body: CommentsSchema.omit({
+                upvotes: true, downvotes: true
+            }),
             response: {
                 201: CommentsSchemaDb,
                 500: z.object({
-                    error: z.string
+                    error: z.string(),
                 }),
             },
         },
     }, async (request, reply) => {
-        const { content, is_best_answer, post_id, user_id, upvotes, downvotes } = request.body
+        const { content, is_best_answer, post_id, user_id } = request.body
         const comment = await prisma.comments.create({
             data: {
                 content,
                 is_best_answer,
                 post_id,
                 user_id,
-                upvotes,
-                downvotes
+            }
+        })
+        if(!comment) {
+            return reply.status(500).send({error: 'Server error'})
+        }
+
+        return reply.status(201).send(comment)
+    })
+
+    app.get('/comments', {
+        schema: {
+            tags: ['Comments'],
+            description: 'List Posts',
+            querystring: PaginationPosts,
+            response: {
+                200: z.array(CommentsSchemaDb),
+                500: z.object({
+                    error: z.string(),
+                })
+            }
+        }
+    }, async (request, reply) => {
+        const { limit, page } = request.query
+        const comments = await prisma.comments.findMany({
+            take: limit,
+            skip: (page - 1) * limit
+        })
+
+        if(!comments) {
+            return reply.status(500).send({error: 'Server error'})
+        }
+            
+        return reply.status(200).send(comments)
+        }
+    )
+
+    app.get('/comments/:id', {
+        schema: {
+            tags: ['Comments'],
+            description: 'Get Comment for id',
+            params: CommentsSchemaDb.pick({
+                id: true
+            }),
+            response: {
+                200: CommentsSchemaDb,
+                500: z.object({
+                    error: z.string()
+                })
+            }
+        }
+    }, async (request, reply) => {
+        const { id } = request.params
+        const comment = await prisma.comments.findUnique({
+            where: {
+                id: id
             }
         })
 
-        return reply.status(201).send(comment)
+        if(!comment){
+            return reply.status(500).send({error: 'Server error'})
+        }
+
+        return reply.status(200).send(comment)
     })
 }
--- apps/api/src/comments/comments.model.ts
+++ apps/api/src/comments/comments.model.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod';
+import z from 'zod';
 
 export const CommentsSchema = z.object({
     content: z.string(),
