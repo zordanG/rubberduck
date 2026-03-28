3065ec027a6909a79c2a0620262f0bff9c0094ca
Wanderson-Valentim <wanderson200108@gmail.com>
2026-03-28 16:13:34 -0300

    feat: add shadcn primitives, sync figma colors and date util (FR0002)

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function dateFormatted(date: string) {
+  return Intl.DateTimeFormat('pt-BR', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  }).format(new Date(date));
+}
