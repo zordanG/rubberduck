826c8b0c3476d0f6da531c42978da7ec84c8798d
thisdev-davi <davisouzaoliveira1975@gmail.com>
2026-04-05 20:45:46 -0300

    wip/FR0003 - testing

--- apps/web/app/post/[slug]/page.tsx
+++ apps/web/app/post/[slug]/page.tsx
@@ -1,8 +1,22 @@
-export default async function SlugPage({
-  params,
-}: {
-  params: Promise<{ slug: string }>
-}) {
+import { CommentsSection } from '@/components/comments/comments-section';
+import { notFound } from 'next/navigation';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3001/api';
+
+export default async function SlugPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
-  return <div className='container mx-auto px-4 py-10'>slug: {slug}</div>;
+
+  const response = await fetch(`${API_URL}/posts/slug/${slug}`, {
+    cache: 'no-store',
+  });
+  if (!response.ok) {
+    notFound(); // tela 404 do nextjs
+  }
+  const post = await response.json();
+
+  return (
+    <div>
+      <CommentsSection postId={post.id} />
+    </div>
+  );
 }
